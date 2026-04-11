95a798bf59762437879473c0e162b4d2b1011656
Mateorh-dev <mateorh.proyectosingsoftware@gmail.com>
2026-04-10 23:52:43 -0500

    creacion componente subpagina

---
+++ hsp/components/SubPagina.tsx
@@ -0,0 +1,43 @@
+import { styles } from "@/assets/styles";
+import { useState } from "react";
+import { Modal, Pressable, Text, View } from "react-native";
+import Titulos from "./Titulos";
+import { Ionicons } from "@expo/vector-icons";
+import { colors } from "@/assets/colors";
+import { dimensions } from "@/assets/dimensions";
+
+type Props = {
+    children: React.ReactNode;
+    tituloPagina: string;
+    textoBotonAbrir?: string;
+    textoBotonCerar?: string;
+};
+
+export default function SubPagina({children: contenido, tituloPagina, textoBotonAbrir, textoBotonCerar="Regresar"}: Props) {
+    const [visivilidadModal, actualizarVisivilidadModal] = useState(false);
+    if (textoBotonAbrir === undefined) {
+        textoBotonAbrir = tituloPagina;
+    }
+    return (
+        <View>
+            <View style={styles.camposTexto}>
+            <Pressable style={styles.botonPrincipal} onPress={() => actualizarVisivilidadModal(true)}>
+                <Text style={styles.contenidoBotonPrincipal}>{textoBotonAbrir}</Text>
+            </Pressable>
+            </View>
+            <Modal
+                visible={visivilidadModal}
+                animationType="slide"
+            >
+                <View style={styles.camposTexto}>
+                <Pressable style={styles.botonSecundario} onPress={() => actualizarVisivilidadModal(false)}>
+                    <Ionicons name="arrow-back-outline" color={colors.primario} size={dimensions.texto.l}/>
+                    <Text style={styles.contenidoBotonSecundario}>{textoBotonCerar}</Text>
+                </Pressable>
+                </View>
+                <Titulos resaltado>{tituloPagina}</Titulos>
+                {contenido}
+            </Modal>
+        </View>
+    );
+}
