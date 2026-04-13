364060823e08da01fc6faca76d6a1f05d17175ac
Mateorh-dev <mateorh.proyectosingsoftware@gmail.com>
2026-04-13 07:59:08 -0500

    correccion componente separadorhorizontal

--- hsp/components/SeparadorHorizontal.tsx
+++ hsp/components/SeparadorHorizontal.tsx
@@ -1,5 +1,5 @@
 import { styles } from "@/assets/styles";
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { View } from "react-native";
 
 export default function SeparadorHorizontal () {
     return (
