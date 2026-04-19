b73e25bcf9b9f3e375ba848fa15195aba94ec402
Mateorh-dev <mateorh.proyectosingsoftware@gmail.com>
2026-04-18 21:57:09 -0500

    actualizacion paginas

--- hsp/app/(tabs)/registrarPaciente.tsx
+++ hsp/app/(tabs)/registrarPaciente.tsx
@@ -1,25 +1,78 @@
 import InputSimple from "@/components/InputSimple";
 import InputFecha from "@/components/InputFecha";
-import { ScrollView } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import SeparadorHorizontal from "@/components/SeparadorHorizontal";
+import { styles } from "@/assets/styles";
+import Titulos from "@/components/Titulos";
 
 export default function PacientesScreen() {
   return (
     <ScrollView>
+      <InputSimple
+      teclado="numeric"
+      limiteCaracteres={15}
+      >
+        No. Identificación
+      </InputSimple>
+      <View style={[styles.camposTexto, styles.botonPrincipal]}>
+        <Text style={styles.contenidoBotonPrincipal}>Consultar</Text>
+      </View>
+
+      <SeparadorHorizontal/>
+
+      <Titulos>Nombre del Paciente</Titulos>
       <InputSimple>Primer nombre</InputSimple>
       <InputSimple>Segundo nombre</InputSimple>
       <InputSimple>Primer apellido</InputSimple>
       <InputSimple>Segundo apellido</InputSimple>
-      <InputSimple teclado="numeric" limiteCaracteres={15}>No. identificación</InputSimple>
-      <InputSimple nombreIcono="map" posicionIcono="fuera">Lugar de expedición</InputSimple>
-      <InputFecha nombreIcono="calendar" posicionIcono="fuera">Fecha nacimiento</InputFecha> 
-      <InputSimple teclado="numeric" limiteCaracteres={5} palceholder="(kg)">Peso</InputSimple>
-      <InputSimple teclado="numeric" limiteCaracteres={15} nombreIcono="call" posicionIcono="fuera">Telefono</InputSimple>
-      <InputSimple teclado="email-address" nombreIcono="mail" posicionIcono="fuera">Correo</InputSimple>
+      
+      <Titulos>Datos de contacto</Titulos>
+      <InputSimple 
+      teclado="numeric"
+      limiteCaracteres={15}
+      nombreIcono="call"
+      posicionIcono="fuera"
+      palceholder="300 000 0000"
+      >
+        Telefono
+      </InputSimple>
+      <InputSimple 
+      teclado="email-address" 
+      nombreIcono="mail" 
+      posicionIcono="fuera"
+      palceholder="micorreo@correos.com"
+      >
+        Correo
+      </InputSimple>
+      
+      <Titulos>Datos Personales</Titulos>
+      <InputSimple 
+      nombreIcono="map" 
+      posicionIcono="fuera"
+      palceholder="Bogota, Bogota DC"
+      >
+        Lugar de expedición Doc. Identidad
+      </InputSimple>
+      <InputFecha 
+      nombreIcono="calendar" 
+      posicionIcono="fuera"
+      >
+        Fecha nacimiento
+      </InputFecha>
+      <InputSimple 
+      teclado="numeric" 
+      limiteCaracteres={5} 
+      palceholder="(kg)"
+      >
+        Peso
+      </InputSimple>
       <InputSimple>Direccion de residencia</InputSimple>
       <InputSimple>Profesion</InputSimple>
       <InputSimple espacioAmplio>Alergias</InputSimple>
       <InputSimple espacioAmplio>Antecedentes</InputSimple>
-
+      <View style={[styles.camposTexto, styles.botonPrincipal]}>
+        <Text style={styles.contenidoBotonPrincipal}>Registrar</Text>
+      </View>
     </ScrollView>
   );
 }
--- hsp/app/(tabs)/registrarTratamiento.tsx
+++ hsp/app/(tabs)/registrarTratamiento.tsx
@@ -1,36 +1,99 @@
 import { colors } from "@/assets/colors";
 import { dimensions } from "@/assets/dimensions";
+import { styles } from "@/assets/styles";
 import InputDesplegable from "@/components/InputDesplegable";
+import InputFecha from "@/components/InputFecha";
 import InputSimple from "@/components/InputSimple";
+import SeparadorHorizontal from "@/components/SeparadorHorizontal";
 import SubPagina from "@/components/SubPagina";
 import Textos from "@/components/Textos";
 import { Ionicons } from "@expo/vector-icons";
-import { ScrollView } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function TratamientosScreen() {
   return (
     <ScrollView>
       <SubPagina tituloPagina="Nuevo Tratamiento">
-        <InputSimple teclado="numeric">No. identificación</InputSimple>
-        <InputSimple>Nombre tratamiento</InputSimple>
+        <InputSimple 
+        teclado="numeric"
+        >
+          No. identificación
+        </InputSimple>
+        <InputSimple
+        nombreIcono="bandage"
+        posicionIcono="fuera"
+        >
+          Nombre tratamiento
+        </InputSimple>
+        <InputFecha
+        nombreIcono="calendar" 
+        posicionIcono="fuera"
+        >
+          Fecha
+        </InputFecha>
+        <View style={[styles.camposTexto, styles.botonPrincipal]}>
+          <Text style={styles.contenidoBotonPrincipal}>Registrar</Text>
+        </View>
       </SubPagina>
+
       <SubPagina tituloPagina="Seguimiento de Tratamiento">
-        <InputSimple teclado="numeric">No. identificación</InputSimple>
-        <InputSimple palceholder="(Cita/Control)" nombreIcono="newspaper" posicionIcono="fuera">Tipo</InputSimple>
-        {/* <InputSimple>Estado</InputSimple> */}
-        <InputDesplegable opciones={["1","2","3","4","5"]} palceholder="Pendiente / Asistio / Reprogramo" nombreIcono="layers" posicionIcono="fuera">Estado (Desplegable)</InputDesplegable>
+        <InputSimple teclado="numeric">
+        No. identificación / Codigo tratamiento
+        </InputSimple>
+        <View style={[styles.camposTexto, styles.botonPrincipal]}>
+          <Text style={styles.contenidoBotonPrincipal}>Consultar</Text>
+        </View>
+        <SeparadorHorizontal/>
+        <InputFecha
+        nombreIcono="calendar" 
+        posicionIcono="fuera"
+        >
+          Fecha
+        </InputFecha>
+        <InputDesplegable
+        opciones={["Cita", "Control"]}
+        palceholder="Cita / Control" 
+        nombreIcono="newspaper" 
+        posicionIcono="fuera"
+        >
+          Tipo
+        </InputDesplegable>
+        <InputDesplegable 
+        opciones={["Pendiente","Asistio","No asistio","Seguimiento sede externa","Reprogramo","Cancelacion"]} 
+        palceholder="Pendiente / Asistio / Reprogramo" 
+        nombreIcono="layers" 
+        posicionIcono="fuera"
+        >
+          Estado
+        </InputDesplegable>
         <InputSimple>Patologia</InputSimple>
-        <InputSimple teclado="numeric" nombreIcono="card" posicionIcono="fuera">Precio</InputSimple>
-        <InputSimple>Tratamiento</InputSimple>
-        <InputSimple>Observaciones</InputSimple>
+        <InputSimple 
+        teclado="numeric" 
+        nombreIcono="card" 
+        posicionIcono="fuera"
+        >
+          Precio
+        </InputSimple>
+        <InputSimple
+        nombreIcono="bandage"
+        posicionIcono="fuera"
+        >
+          Tratamiento
+        </InputSimple>
+        <InputSimple espacioAmplio>Observaciones</InputSimple>
         <Textos>
           Registro Fotografico
         </Textos>
+        <View style={styles.camposTexto}>
         <Ionicons
           name={"image-outline"}
           size={dimensions.figura.l}
           color={colors.primario}
         />
+        </View>
+        <View style={[styles.camposTexto, styles.botonPrincipal]}>
+          <Text style={styles.contenidoBotonPrincipal}>Registrar</Text>
+        </View>
       </SubPagina>
     </ScrollView>
   );
